e7545781cc6ba63d9ed5d1d3f987760707fb7e98
Pepijn Goorden <pepijngoorden@gmail.com>
2025-07-31 14:16:22 +0200

    vite package-lock update

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,11 @@ export default defineConfig(({ command, mode }) => {
         },
         plugins: [
             laravel({
-                refresh: true,
+                refresh: [
+                    'resources/views/**/*.blade.php',      // Laravel Blade views
+                    'resources/views/**/*.antlers.html', // Statamic Antlers templates (if using)
+                    'content/**/*.{md,mdx}',               // Statamic content changes
+                ],
                 input: [
                     'resources/css/site.scss',
                     'resources/js/site.js',
